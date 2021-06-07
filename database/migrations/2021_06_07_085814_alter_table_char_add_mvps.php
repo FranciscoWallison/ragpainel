6a79a558ce54740198acc54d1a77c57f9cd00569
Rafael Santos <raffrenan@gmail.com>
2021-06-07 19:52:14 -0300

    Corrigido migration de mvps.

--- database/migrations/2021_06_07_085814_alter_table_char_add_mvps.php
+++ database/migrations/2021_06_07_085814_alter_table_char_add_mvps.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterTableLoginAddMvps extends Migration
+class AlterTableCharAddMvps extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AlterTableLoginAddMvps extends Migration
      */
     public function up()
     {
-        Schema::table('login', function (Blueprint $table) {
+        Schema::table('char', function (Blueprint $table) {
             $table->integer('mvps')->nullable();
         });
     }
@@ -25,7 +25,7 @@ class AlterTableLoginAddMvps extends Migration
      */
     public function down()
     {
-        Schema::table('login', function (Blueprint $table) {
+        Schema::table('char', function (Blueprint $table) {
             $table->dropColumn('mvps');
         });
     }
