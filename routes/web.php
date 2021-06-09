9e0d19733e6d37ddbf5b5b462e1c8bc0767fd2a1
Rafael Santos <raffrenan@gmail.com>
2021-06-09 20:10:44 -0300

    Adicionado verificação de e-mail em todas as rotas.

--- routes/web.php
+++ routes/web.php
@@ -72,19 +72,19 @@ Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $requ
 
 Route::post('/email/verification-notification', [RegisteredUserController::class, 'verification'])->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
-Route::get('/myaccount',[MyAccount::class, 'index'])->middleware('auth', 'verified')->name('user.myaccount');
-Route::post('/myaccount',[MyAccount::class, 'update'])->middleware('auth')->name('user.myaccount.update');
-Route::post('/myaccount/upload',[MyAccount::class, 'uploadimg'])->middleware('auth')->name('user.myaccount.upload');
-Route::get('/mychars',[MyChars::class, 'index'])->middleware('auth')->name('user.mychars');
-Route::post('/mychars/resetposition',[MyChars::class, 'resetPosition'])->middleware('auth')->name('user.resetposition');
-Route::post('/mychars/resetstyle',[MyChars::class, 'resetStyle'])->middleware('auth')->name('user.resetstyle');
+Route::get('/myaccount',[MyAccount::class, 'index'])->middleware('auth', 'verify')->name('user.myaccount');
+Route::post('/myaccount',[MyAccount::class, 'update'])->middleware('auth', 'verify')->name('user.myaccount.update');
+Route::post('/myaccount/upload',[MyAccount::class, 'uploadimg'])->middleware('auth', 'verify')->name('user.myaccount.upload');
+Route::get('/mychars',[MyChars::class, 'index'])->middleware('auth', 'verify')->name('user.mychars');
+Route::post('/mychars/resetposition',[MyChars::class, 'resetPosition'])->middleware('auth', 'verify')->name('user.resetposition');
+Route::post('/mychars/resetstyle',[MyChars::class, 'resetStyle'])->middleware('auth', 'verify')->name('user.resetstyle');
 
 // Tickets
-Route::get('/tickets',[TicketsController::class, 'index'])->middleware('auth')->name('tickets.index');
-Route::post('/tickets',[TicketsController::class, 'send'])->middleware('auth')->name('tickets.send');
-Route::get('/tickets/my',[TicketsController::class, 'myTickets'])->middleware('auth')->name('tickets.mytickets');
+Route::get('/tickets',[TicketsController::class, 'index'])->middleware('auth', 'verify')->name('tickets.index');
+Route::post('/tickets',[TicketsController::class, 'send'])->middleware('auth', 'verify')->name('tickets.send');
+Route::get('/tickets/my',[TicketsController::class, 'myTickets'])->middleware('auth', 'verify')->name('tickets.mytickets');
 Route::get('/tickets/view/{id}',[TicketsController::class, 'ticketView'])->middleware('auth')->name('tickets.ticketview');
-Route::post('/tickets/reply',[TicketsController::class, 'reply'])->middleware('auth')->name('tickets.reply');
+Route::post('/tickets/reply',[TicketsController::class, 'reply'])->middleware('auth', 'verify')->name('tickets.reply');
 
 // Rankings
 Route::get('/rankings/woe',[GVGController::class, 'index'])->name('rankings.woe');
@@ -101,40 +101,40 @@ Route::post('/database/monster', [DatabaseController::class, 'monsterSearch'])->
 
 // Administrador.
 // Logs
-Route::get('/admin/logs',[LogsController::class, 'index'])->middleware('auth', 'admin')->name('admin.logs');
+Route::get('/admin/logs',[LogsController::class, 'index'])->middleware('auth', 'admin', 'verify')->name('admin.logs');
 // Gerenciar Cash
-Route::get('/admin/managercash',[ManagerCashController::class, 'index'])->middleware('auth', 'admin')->name('admin.managercash');
-Route::post('/admin/managercash/add',[ManagerCashController::class, 'add'])->middleware('auth', 'admin')->name('admin.managercash.add');
-Route::post('/admin/managercash/remove',[ManagerCashController::class, 'remove'])->middleware('auth', 'admin')->name('admin.managercash.remove');
-Route::post('/admin/managercash',[ManagerCashController::class, 'find'])->middleware('auth', 'admin')->name('admin.managercash.find');
+Route::get('/admin/managercash',[ManagerCashController::class, 'index'])->middleware('auth', 'admin', 'verify')->name('admin.managercash');
+Route::post('/admin/managercash/add',[ManagerCashController::class, 'add'])->middleware('auth', 'admin', 'verify')->name('admin.managercash.add');
+Route::post('/admin/managercash/remove',[ManagerCashController::class, 'remove'])->middleware('auth', 'admin', 'verify')->name('admin.managercash.remove');
+Route::post('/admin/managercash',[ManagerCashController::class, 'find'])->middleware('auth', 'admin', 'verify')->name('admin.managercash.find');
 // Gerenciar VIP
-Route::get('/admin/managervip',[ManagerVipController::class, 'index'])->middleware('auth', 'admin')->name('admin.managervip');
-Route::post('/admin/managervip/add',[ManagerVipController::class, 'add'])->middleware('auth', 'admin')->name('admin.managervip.add');
-Route::post('/admin/managervip/remove',[ManagerVipController::class, 'remove'])->middleware('auth', 'admin')->name('admin.managervip.remove');
-Route::post('/admin/managervip',[ManagerVipController::class, 'find'])->middleware('auth', 'admin')->name('admin.managervip.find');
+Route::get('/admin/managervip',[ManagerVipController::class, 'index'])->middleware('auth', 'admin', 'verify')->name('admin.managervip');
+Route::post('/admin/managervip/add',[ManagerVipController::class, 'add'])->middleware('auth', 'admin', 'verify')->name('admin.managervip.add');
+Route::post('/admin/managervip/remove',[ManagerVipController::class, 'remove'])->middleware('auth', 'admin', 'verify')->name('admin.managervip.remove');
+Route::post('/admin/managervip',[ManagerVipController::class, 'find'])->middleware('auth', 'admin', 'verify')->name('admin.managervip.find');
 // Gerenciar Equipe
-Route::get('/admin/managerstaff',[ManagerStaffController::class, 'index'])->middleware('auth', 'admin')->name('admin.managerstaff');
-Route::post('/admin/managerstaff/add',[ManagerStaffController::class, 'add'])->middleware('auth', 'admin')->name('admin.managerstaff.add');
-Route::post('/admin/managerstaff/remove',[ManagerStaffController::class, 'remove'])->middleware('auth', 'admin')->name('admin.managerstaff.remove');
-Route::post('/admin/managerstaff',[ManagerStaffController::class, 'find'])->middleware('auth', 'admin')->name('admin.managerstaff.find');
+Route::get('/admin/managerstaff',[ManagerStaffController::class, 'index'])->middleware('auth', 'admin', 'verify')->name('admin.managerstaff');
+Route::post('/admin/managerstaff/add',[ManagerStaffController::class, 'add'])->middleware('auth', 'admin', 'verify')->name('admin.managerstaff.add');
+Route::post('/admin/managerstaff/remove',[ManagerStaffController::class, 'remove'])->middleware('auth', 'admin', 'verify')->name('admin.managerstaff.remove');
+Route::post('/admin/managerstaff',[ManagerStaffController::class, 'find'])->middleware('auth', 'admin', 'verify')->name('admin.managerstaff.find');
 // Gerenciar Punições
-Route::get('/admin/managerban',[ManagerBanController::class, 'index'])->middleware('auth', 'admin')->name('admin.managerban');
-Route::post('/admin/managerban/add',[ManagerBanController::class, 'add'])->middleware('auth', 'admin')->name('admin.managerban.add');
-Route::post('/admin/managerban/remove',[ManagerBanController::class, 'remove'])->middleware('auth', 'admin')->name('admin.managerban.remove');
-Route::post('/admin/managerban',[ManagerBanController::class, 'find'])->middleware('auth', 'admin')->name('admin.managerban.find');
+Route::get('/admin/managerban',[ManagerBanController::class, 'index'])->middleware('auth', 'admin', 'verify')->name('admin.managerban');
+Route::post('/admin/managerban/add',[ManagerBanController::class, 'add'])->middleware('auth', 'admin', 'verify')->name('admin.managerban.add');
+Route::post('/admin/managerban/remove',[ManagerBanController::class, 'remove'])->middleware('auth', 'admin', 'verify')->name('admin.managerban.remove');
+Route::post('/admin/managerban',[ManagerBanController::class, 'find'])->middleware('auth', 'admin', 'verify')->name('admin.managerban.find');
 // Gerenciar Tickets.
-Route::get('/admin/managertickets',[ManagerTicketController::class, 'index'])->middleware('auth', 'admin')->name('admin.managertickets');
-Route::get('/admin/managertickets/view/{id}', [ManagerTicketController::class, 'view'])->middleware('auth', 'admin')->name('admin.managertickets.view');
-Route::match(['post', 'get'], '/admin/managertickets/close/{id}',[ManagerTicketController::class, 'close'])->middleware('auth', 'admin')->name('admin.managertickets.close');
-Route::match(['post', 'get'], '/admin/managertickets/open/{id}',[ManagerTicketController::class, 'open'])->middleware('auth', 'admin')->name('admin.managertickets.open');
-Route::post('/admin/managertickets/reply', [ManagerTicketController::class, 'reply'])->middleware('auth', 'admin')->name('admin.managertickets.reply');
+Route::get('/admin/managertickets',[ManagerTicketController::class, 'index'])->middleware('auth', 'admin', 'verify')->name('admin.managertickets');
+Route::get('/admin/managertickets/view/{id}', [ManagerTicketController::class, 'view'])->middleware('auth', 'admin', 'verify')->name('admin.managertickets.view');
+Route::match(['post', 'get'], '/admin/managertickets/close/{id}',[ManagerTicketController::class, 'close'])->middleware('auth', 'admin', 'verify')->name('admin.managertickets.close');
+Route::match(['post', 'get'], '/admin/managertickets/open/{id}',[ManagerTicketController::class, 'open'])->middleware('auth', 'admin', 'verify')->name('admin.managertickets.open');
+Route::post('/admin/managertickets/reply', [ManagerTicketController::class, 'reply'])->middleware('auth', 'admin', 'verify')->name('admin.managertickets.reply');
 // Configurações do Painel
-Route::get('/admin/configs',[ConfigController::class, 'index'])->middleware('auth', 'admin')->name('admin.config');
-Route::post('/admin/configs/savegeneral',[ConfigController::class, 'saveGeneral'])->middleware('auth', 'admin')->name('admin.config.savegeneral');
-Route::post('/admin/configs/saveaccount',[ConfigController::class, 'saveAccount'])->middleware('auth', 'admin')->name('admin.config.saveaccount');
-Route::post('/admin/configs/savecolor',[ConfigController::class, 'saveColor'])->middleware('auth', 'admin')->name('admin.config.savecolor');
-Route::post('/admin/configs/savecolorbg',[ConfigController::class, 'saveColorBg'])->middleware('auth', 'admin')->name('admin.config.savecolorbg');
-Route::post('/admin/configs/savevip',[ConfigController::class, 'saveVip'])->middleware('auth', 'admin')->name('admin.config.savevip');
-Route::post('/admin/configs/savestaff',[ConfigController::class, 'saveStaff'])->middleware('auth', 'admin')->name('admin.config.savestaff');
-Route::post('/admin/configs/categorys/add',[ConfigController::class, 'addCategory'])->middleware('auth', 'admin')->name('admin.config.addcategory');
-Route::post('/admin/configs/categorys/remove',[ConfigController::class, 'removeCategory'])->middleware('auth', 'admin')->name('admin.config.removecategory');
+Route::get('/admin/configs',[ConfigController::class, 'index'])->middleware('auth', 'admin', 'verify')->name('admin.config');
+Route::post('/admin/configs/savegeneral',[ConfigController::class, 'saveGeneral'])->middleware('auth', 'admin', 'verify')->name('admin.config.savegeneral');
+Route::post('/admin/configs/saveaccount',[ConfigController::class, 'saveAccount'])->middleware('auth', 'admin', 'verify')->name('admin.config.saveaccount');
+Route::post('/admin/configs/savecolor',[ConfigController::class, 'saveColor'])->middleware('auth', 'admin', 'verify')->name('admin.config.savecolor');
+Route::post('/admin/configs/savecolorbg',[ConfigController::class, 'saveColorBg'])->middleware('auth', 'admin', 'verify')->name('admin.config.savecolorbg');
+Route::post('/admin/configs/savevip',[ConfigController::class, 'saveVip'])->middleware('auth', 'admin', 'verify')->name('admin.config.savevip');
+Route::post('/admin/configs/savestaff',[ConfigController::class, 'saveStaff'])->middleware('auth', 'admin', 'verify')->name('admin.config.savestaff');
+Route::post('/admin/configs/categorys/add',[ConfigController::class, 'addCategory'])->middleware('auth', 'admin', 'verify')->name('admin.config.addcategory');
+Route::post('/admin/configs/categorys/remove',[ConfigController::class, 'removeCategory'])->middleware('auth', 'admin', 'verify')->name('admin.config.removecategory');
